b1460887768f8f172194329919acc15203268662
Josue Zamudio <jozamudi@slac.stanford.edu>
2025-06-12 08:52:17 -0700

    build ioc

--- iocBoot/ioc-sample-delivery/st.cmd
+++ iocBoot/ioc-sample-delivery/st.cmd
@@ -4,8 +4,8 @@
 #         Project: lcls-plc-rix-sample-delivery.tsproj
 #        PLC name: sample_delivery (sample_delivery Instance)
 # Generated using: pytmc 2.17.0
-# Project version: 7f75f5f
-#    Project hash: 7f75f5f1992466b420748acaa4ef49c8bcfef586
+# Project version: f0da7aa
+#    Project hash: f0da7aa54f6dc201efb99463bec75c74d23b836a
 #     PLC IP/host: 172.21.140.88
 #      PLC Net ID: 172.21.140.88.1.1
 # ** DEVELOPMENT MODE IOC **
@@ -37,7 +37,7 @@ epicsEnvSet("ASYN_PORT",        "ASYN_PLC")
 epicsEnvSet("IPADDR",           "172.21.140.88")
 epicsEnvSet("AMSID",            "172.21.140.88.1.1")
 epicsEnvSet("AMS_PORT",         "851")
-epicsEnvSet("ADS_MAX_PARAMS",   "1038")
+epicsEnvSet("ADS_MAX_PARAMS",   "1047")
 epicsEnvSet("ADS_SAMPLE_MS",    "50")
 epicsEnvSet("ADS_MAX_DELAY_MS", "100")
 epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
@@ -83,7 +83,7 @@ dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:sample_delive
 dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:sample_delivery,IDX=2,TASK_PORT=350")
 dbLoadRecords("TwinCAT_AppInfo.db", "PORT=$(ASYN_PORT), PREFIX=PLC:sample_delivery")
 
-dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:sample_delivery,PROJECT=lcls-plc-rix-sample-delivery.tsproj,HASH=7f75f5f,VERSION=7f75f5f,PYTMC=2.17.0,PLC_HOST=172.21.140.88")
+dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:sample_delivery,PROJECT=lcls-plc-rix-sample-delivery.tsproj,HASH=f0da7aa,VERSION=f0da7aa,PYTMC=2.17.0,PLC_HOST=172.21.140.88")
 
 #   lcls-twincat-sample-delivery: * -> 1.0.0 (SLAC)
 dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:sample_delivery,DEPENDENCY=lcls-twincat-sample-delivery,VERSION=1.0.0,VENDOR=SLAC")
@@ -99,8 +99,8 @@ cd "$(IOC_TOP)"
 ## PLC Project Database files ##
 dbLoadRecords("sample_delivery.db", "PORT=$(ASYN_PORT),PREFIX=PLC:sample_delivery:,IOCNAME=$(IOC),IOC=$(IOC)")
 
-# Total records: 38
-callbackSetQueueSize(2076)
+# Total records: 47
+callbackSetQueueSize(2094)
 
 # Autosave and archive settings:
 save_restoreSet_status_prefix("PLC:sample_delivery:")
